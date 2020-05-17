278ffc6d0146081ed6329b5e123707fed6cdd9b9
Иван Волокитин <ivanvolokitin@gmail.com>
2020-05-17 20:39:12 +0300

    oh yeah

--- finnobachi.py
+++ finnobachi.py
@@ -4,7 +4,6 @@ def fib1 (n : int) -> int:
         return n 
     return  fib1(n - 2) + fib1(n - 1)
 
-if __name__ == "__main__":
-    print(fib1(5))
-    print(fib1(10))
-    print (fib1(10))
+print(fib1(5))
+print(fib1(10))
+print (fib1(20))
